fce585f964e0921b0b101fe6cc4c8f67b22fe5aa
CANDELY001 <candely1charifa@gmail.com>
2023-04-22 14:34:12 -0700

    candely task 0

--- main.c
+++ main.c
@@ -9,14 +9,11 @@
  */
 int main(void)
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
-
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
-
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
 
 	return (0);
 }
