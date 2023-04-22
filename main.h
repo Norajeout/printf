691cdc428afd88d351ae818a6015974cb3ba95e7
Norajeout <norajeout123@gmail.com>
2023-04-22 13:43:59 -0700

    nora

--- main.h
+++ main.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 
 int _printf(const char *format, ...);
 int check(va_list list, char c);
 int _putchar(char c);
 int check(va_list list, char c);
-int _print_string(char *s);
 
 #endif
