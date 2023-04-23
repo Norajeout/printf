e2434d25d5b79713a1dba10612bdbaae0cd58edc
CANDELY001 <candely1charifa@gmail.com>
2023-04-23 01:14:05 -0700

    candely task 0

--- printf.c
+++ printf.c
@@ -2,31 +2,31 @@
 
 /* BY NORA JEOUT & CHARIFA MASBAHI*/
 /**
-* _printf - function that produces output according to a format
-* @format: format
-* @... : parameters
-* Return: the number of characters printed
-*/
+ * _printf - function that produces output according to a format
+ * @format: format
+ * @... : parameters
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list ap;
-        int i = 0, c = 0;
+	int i = 0, c = 0;
 
-        if (!format)
-        {
-                return (-1);
-        }
-        va_start(ap, format);
-        while (format[i])
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        c += check_format(ap, format[i]);
-                } else
-                        c += _putchar(format[i]);
-                i++;
-        }
-        va_end(ap);
-        return (c);
+	if (!format)
+	{
+		return (-1);
+	}
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			c += check_format(ap, format[i]);
+		} else
+			c += _putchar(format[i]);
+		i++;
+	}
+	va_end(ap);
+	return (c);
 }
