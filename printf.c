9f7fe2c2d89fa795ddfc9468ffa29bd50c57a7df
Norajeout <norajeout123@gmail.com>
2023-04-22 20:22:03 -0700

    last commit

--- printf.c
+++ printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
                 return (-1);
         }
         va_start(ap, format);
-        while (format[i])
+        for (i = 0; format && format[i] != '\0'; i++)
         {
                 if (format[i] == '%')
                 {
