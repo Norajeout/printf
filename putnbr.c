f41d9605a129bc17c99dafb634a7d23984917be6
CANDELY001 <candely1charifa@gmail.com>
2023-04-23 08:05:35 -0700

    task 0

--- putnbr.c
+++ putnbr.c
@@ -8,12 +8,19 @@ int _putnbr(int n)
 {
 	int c = 0;
 
-	if (n < 0)
+	if (n == INT_MIN)
+	{
+		c += _putchar('-');
+		_putchar('2');
+		c += _putnbr(147483648);
+	}
+	else if (n < 0)
 	{
 		c += _putchar('-');
 		n = -n;
+		c += _putnbr(n);
 	}
-	if (n > 9)
+	else if (n > 9)
 	{
 		c += _putnbr(n / 10);
 		c += _putnbr(n % 10);
