2df9e4cc03417a3478187f9e5f2b024f6eb29892
CANDELY001 <candely1charifa@gmail.com>
2023-04-23 13:46:21 -0700

    0-1

--- putnbr.c
+++ putnbr.c
@@ -9,17 +9,19 @@ int _putnbr(int n)
 {
 	int c = 0;
 
-	if (n < 0)
+	if (n == INT_MIN)
 	{
 		c += _putchar('-');
-		if (n == -2147483648)
-		{
-			c += _putchar('2');
-			n %= 1000000000;
-		}
-		n = -n;
+		c += _putchar('2');
+		c += _putnbr(147483648);
 	}
-	if (n > 9)
+	else if (n < 0)
+	{
+		c += _putchar('-');
+		n = -n;
+		c += _putnbr(n);
+	}
+	else if (n > 9)
 	{
 		c += _putnbr(n / 10);
 		c += _putnbr(n % 10);
