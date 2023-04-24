622db71555dec561d0639a6d8dd0d0d995209da5
CANDELY001 <candely1charifa@gmail.com>
2023-04-24 12:17:36 -0700

    task 1

--- printf.c
+++ printf.c
@@ -16,8 +16,6 @@ int _printf(const char *format, ...)
 	{
 		return (-1);
 	}
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
 	va_start(ap, format);
 	while (format[i])
 	{
