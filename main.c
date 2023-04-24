24a9eef06a77287a58cf719e6792b8194051088f
CANDELY001 <candely1charifa@gmail.com>
2023-04-24 04:24:25 -0700

    baraka

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <limits.h>
+#include <limits.h> 
 #include <stdio.h>
 #include "main.h"
 
@@ -38,5 +38,7 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
+	_printf("Hello %%s\n");
+	printf("Hello %%s\n");
 	return (0);
 }
