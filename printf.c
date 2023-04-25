71815e36dd3ce135b409e054622172e8948b793d
Norajeout <norajeout123@gmail.com>
2023-04-24 20:20:37 -0700

    printini

--- printf.c
+++ printf.c
@@ -24,6 +24,10 @@ int _printf(const char *format, ...)
 			i++;
 			if (format[i] == '\0')
 				return (-1);
+			else if (format[i] == ' ')
+			{
+				return (-1);
+			}
 			c += check_format(ap, format[i]);
 
 		} else
